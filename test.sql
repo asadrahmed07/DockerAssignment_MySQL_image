34f237d5ed084cb25ccffb4246a315b167db5583
Asad Ahmed <asadrahmed@gmail.com>
2022-01-06 21:33:46 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,6 @@
+CREATE table studentData (Name varchar(25),RollNo integer,Address varchar(50),MobileNumber varchar(20),PanNumber varchar(20)); 
+
+INSERT into studentData values('Asad' ,20112028 ,'New Delhi' ,'7977835891' ,'SMX56249633');
+INSERT into studentData values('Chinmay' ,20112025 ,'Pune' ,'9845136145' ,'AHD56249633');
+INSERT into studentData values('Aalhad' ,20112023 ,'Mumbai' ,'9912214502' ,'SHD56249545');
+INSERT into studentData values('Manish' ,20112022 ,'Solapur' ,'7824523654' ,'RTH45129633');
